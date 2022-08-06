7a61fd0baa8d96bd157bda7288bf6ffe8e8969ba
Ravi kiran Machha <92377897+Ravi14143@users.noreply.github.com>
2022-08-06 18:20:53 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,5 @@
 <?php
-  $conn=mysqli_connect("localhost","root","");
+  $conn=mysqli_connect("remotemysql.com","iPxJWxHgpd","eY9bRPcgPy");
   $db=mysqli_select_db($conn,"cms");
   if(!$conn){
       echo "<script>alert('connection failed');</script>";
